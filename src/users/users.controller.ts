8db3ac335ff247ffe886c974934145411f0dc8d5
edmorua <eduardo.r.morua@gmail.com>
2026-02-17 15:55:58 -0600

    add crud for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UsersService } from './users.service';
+import { UpdateUserRequest } from './dto/update-user.request';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +19,28 @@ export class UsersController {
   @Post()
   createUser(@Body() request: CreateUserRequest) {
     return this.usersService.createUser(request);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getUserById(id);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() request: UpdateUserRequest,
+  ) {
+    return this.usersService.updateUser(id, request);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { CreateUserRequest } from './dto/create-user.request';
 import { PrismaService } from '../prisma/prisma.service';
-import { User } from 'src/generated/prisma/client';
+import { UpdateUserRequest } from './dto/update-user.request';
+import { UserResponse } from './dto/user.response';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createUser(request: CreateUserRequest): Promise<User> {
-    console.log({ request });
+  async createUser(request: CreateUserRequest): Promise<UserResponse> {
     const newUser = await this.prismaService.user.create({
       data: {
         name: request.name,
         email: request.email,
         password: await bcrypt.hash(request.password, 10),
       },
+      omit: {
+        password: true,
+      },
     });
-    console.log({ newUser });
     return newUser;
+  }
+
+  async updateUser(
+    id: number,
+    request: UpdateUserRequest,
+  ): Promise<UserResponse> {
+    await this.getUserById(id);
+    const updatedUser = await this.prismaService.user.update({
+      where: { id },
+      data: {
+        ...(request.name && { name: request.name }),
+        ...(request.email && { email: request.email }),
+        ...(request.password && {
+          password: await bcrypt.hash(request.password, 10),
+        }),
+      },
+      omit: {
+        password: true,
+      },
+    });
+    return updatedUser;
+  }
+
+  async getUserById(id: number): Promise<UserResponse> {
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+      omit: {
+        password: true,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
+  }
+
+  async getAllUsers(): Promise<UserResponse[]> {
+    const users = await this.prismaService.user.findMany({
+      omit: {
+        password: true,
+      },
+    });
+    return users;
+  }
+
+  async deleteUser(id: number): Promise<void> {
+    await this.getUserById(id);
+    await this.prismaService.user.delete({
+      where: { id },
+    });
   }
 }
